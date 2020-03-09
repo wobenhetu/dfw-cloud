94edee1b265fed6ffc8a09afe9aaf59f9819a0de
dfw <dfwmaomao@163.com>
2020-03-09 21:57:52 +0800

    个人网盘开发

---
+++ src/main/java/com/dfw/MotiCloudApplication.java
@@ -0,0 +1,12 @@
+package com.dfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotiCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotiCloudApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dfw/service/impl/FileStoreServiceImpl.java
@@ -0,0 +1,45 @@
+package com.dfw.service.impl;
+
+import com.dfw.entity.FileStore;
+import com.dfw.service.FileStoreService;
+import com.dfw.utils.LogUtils;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName: FileStoreServiceImpl
+ * @Description: 文件仓库业务层接口实现类
+ * @author: xw
+ * @date 2020/1/26 22:16
+ * @Version: 1.0
+ **/
+@Service
+public class FileStoreServiceImpl extends BaseService implements FileStoreService {
+
+    Logger logger = LogUtils.getInstance(UserServiceImpl.class);
+
+    @Override
+    public Integer addFileStore(FileStore fileStore) {
+        return fileStoreMapper.addFileStore(fileStore);
+    }
+
+    @Override
+    public FileStore getFileStoreByUserId(Integer userId) {
+        return fileStoreMapper.getFileStoreByUserId(userId);
+    }
+
+    @Override
+    public FileStore getFileStoreById(Integer fileStoreId) {
+        return fileStoreMapper.getFileStoreById(fileStoreId);
+    }
+
+    @Override
+    public Integer addSize(Integer id, Integer size) {
+        return fileStoreMapper.addSize(id,size);
+    }
+
+    @Override
+    public Integer subSize(Integer id, Integer size) {
+        return fileStoreMapper.subSize(id,size);
+    }
+}
